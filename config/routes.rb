88d9d63ee241bef3c448f2a5685d818b9a2846a9
Christian Lazo <lazochristian.cl@gmail.com>
2017-08-19 19:12:03 +0000

    Ability to delete users

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   as :user do
     get 'admin/new', to: 'admin#new', as: 'admin_new'
     post 'admin/create', to: 'admin#create', as: 'admin_create'
+    match 'admin/:id', to: 'admin#destroy', via: :delete, as: :admin_destroy_user
   end
   
   devise_scope :user do 
     get "/admin/dashoboard" => "admin#dashboard", as: 'admin_dashboard'
     get '/admin', to: 'admin#index'
     get 'user/:id', to: 'admin#show', as: 'user'
-    post 'admin/:id', to: 'admin#destroy'
   end
   
   get '/data_entry', to: 'static#data_entry'
