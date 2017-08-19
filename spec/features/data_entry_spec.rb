9be1ea9024b29bfbbaf5794d008f1127d054e042
Christian Lazo <lazochristian.cl@gmail.com>
2017-08-19 18:51:40 +0000

    Refactor data entry spec

--- spec/features/data_entry_spec.rb
+++ spec/features/data_entry_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 describe 'data entry page' do
   before do 
-      @user = @user = User.create(email: "user@user.com", password: "asdfasdf", admin: true)
-      login_as(@user, :scope => :user)
-      visit data_entry_path
-    end
+    @user = @user = User.create(email: "user@user.com", password: "asdfasdf", admin: true)
+    login_as(@user, :scope => :user)
+    visit data_entry_path
+  end
+  
+  before do 
+    @speciman = Speciman.create(pass_or_fail: "pass", test_plan: "4FR4132-12-FA1", cushion: "Back and Bottom", test_plan_id: "1", seat_model: 4132, test_date: Date.today, vert_burn_length_front: 4.8, vert_burn_length_back: 0.0, horiz_burn_length_top: 5.2, horiz_burn_length_bottom: 9.5, weight_loss_percent: 8.23, weight_loss_lbs: 0.42, cover_config: "Fully Encapsulated", foam_a_material: "DAX 45 Polyurethane", foam_a_percent: 100.00, decorative_insulation_part_number: "71000", decorative_insulation_mfr: nil, backing: nil, adhesive_one: "Simalfa 309", adhesive_two: nil, fireblock_part_number: "413")
+  end
     
   describe 'navigation' do 
     it 'can be reached sucessfully' do 
@@ -34,14 +38,11 @@ describe 'data entry page' do
       fill_in 'speciman[test_plan]', with: "4FR4132-00"
       click_on "Submit"
       
-      #expect(page).to have_content("4FR4132-00")
+       # refactor later
     end
   end
   
   describe 'show' do 
-    before do 
-      @speciman = Speciman.create(pass_or_fail: "pass", test_plan: "4FR4132-12-FA1", cushion: "Back and Bottom", test_plan_id: "1", seat_model: 4132, test_date: Date.today, vert_burn_length_front: 4.8, vert_burn_length_back: 0.0, horiz_burn_length_top: 5.2, horiz_burn_length_bottom: 9.5, weight_loss_percent: 8.23, weight_loss_lbs: 0.42, cover_config: "Fully Encapsulated", foam_a_material: "DAX 45 Polyurethane", foam_a_percent: 100.00, decorative_insulation_part_number: "71000", decorative_insulation_mfr: nil, backing: nil, adhesive_one: "Simalfa 309", adhesive_two: nil, fireblock_part_number: "413") 
-    end
     it 'links to show page index' do 
       visit specimen_path
       click_link('4FR4132-12-FA1')
@@ -51,10 +52,6 @@ describe 'data entry page' do
   end
   
   describe 'edit' do
-    before do 
-      @speciman = Speciman.create(pass_or_fail: "pass", test_plan: "4FR4132-12-FA1", cushion: "Back and Bottom", test_plan_id: "1", seat_model: 4132, test_date: Date.today, vert_burn_length_front: 4.8, vert_burn_length_back: 0.0, horiz_burn_length_top: 5.2, horiz_burn_length_bottom: 9.5, weight_loss_percent: 8.23, weight_loss_lbs: 0.42, cover_config: "Fully Encapsulated", foam_a_material: "DAX 45 Polyurethane", foam_a_percent: 100.00, decorative_insulation_part_number: "71000", decorative_insulation_mfr: nil, backing: nil, adhesive_one: "Simalfa 309", adhesive_two: nil, fireblock_part_number: "413")
-    end
-    
     it 'allows admin to edit the material page' do 
       visit edit_speciman_path(@speciman)
       
@@ -67,9 +64,6 @@ describe 'data entry page' do
   end
   
   describe 'delete' do 
-    before do 
-      @speciman = Speciman.create(pass_or_fail: "pass", test_plan: "4FR4132-12-FA1", cushion: "Back and Bottom", test_plan_id: "1", seat_model: 4132, test_date: Date.today, vert_burn_length_front: 4.8, vert_burn_length_back: 0.0, horiz_burn_length_top: 5.2, horiz_burn_length_bottom: 9.5, weight_loss_percent: 8.23, weight_loss_lbs: 0.42, cover_config: "Fully Encapsulated", foam_a_material: "DAX 45 Polyurethane", foam_a_percent: 100.00, decorative_insulation_part_number: "71000", decorative_insulation_mfr: nil, backing: nil, adhesive_one: "Simalfa 309", adhesive_two: nil, fireblock_part_number: "413")
-    end
     it 'can be deleted' do 
       visit speciman_path(@speciman)
       
