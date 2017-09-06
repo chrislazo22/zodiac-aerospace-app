1e84866a85662f91797309782e0b3d1040ce4bd6
Christian Lazo <lazochristian.cl@gmail.com>
2017-09-06 01:03:58 +0000

    Added search model and controller for advanced search

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728191952) do
+ActiveRecord::Schema.define(version: 20170906010300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "searches", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "specimen", force: :cascade do |t|
     t.string   "pass_or_fail"
