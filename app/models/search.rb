291e5a5bb510633b2ba3e645513e18d971be1a21
Christian Lazo <lazochristian.cl@gmail.com>
2017-09-10 20:21:19 +0000

    Add show page to advanced search fucntion

--- app/models/search.rb
+++ app/models/search.rb
@@ -9,6 +9,7 @@ class Search < ActiveRecord::Base
   
   def find_specimen
     specimen = Speciman.order(:test_plan)
-    specimen = specimen.where("test_plan like ?", "%#{keywords}%") if keywords.present?
+    specimen = specimen.where("test_plan like ?", "%#{test_plan_search}%") if test_plan_serach.present?
+    specimen
   end
 end
