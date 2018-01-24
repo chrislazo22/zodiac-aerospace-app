b8a09eb7df6cea05494ec0cd9c9a54d7be1ffe3b
Christian Lazo <lazochristian.cl@gmail.com>
2018-01-23 20:55:17 -0800

    More Updates

--- app/controllers/specimen_controller.rb
+++ app/controllers/specimen_controller.rb
@@ -1,5 +1,5 @@
 class SpecimenController < ApplicationController
-before_action :set_specimen, only: [:show, :edit, :update, :destroy] 
+before_action :set_specimen, only: [:show, :edit, :update, :destroy]
 
   def index
     @specimen = Speciman.all.page(params[:page]).per_page(25)
@@ -7,7 +7,7 @@ before_action :set_specimen, only: [:show, :edit, :update, :destroy]
       @specimen = Speciman.search(params[:search]).page(params[:page]).per_page(25)
     end
     @x = 0
-    
+
     respond_to do |format|
       format.html
       format.xls  { send_data @specimen.to_csv(col_sep: "\t") }
@@ -17,14 +17,14 @@ before_action :set_specimen, only: [:show, :edit, :update, :destroy]
   def new
     @specimen = Speciman.new
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
-  def create 
+
+  def create
     @specimen = Speciman.new(specimen_params)
     if @specimen.save
       redirect_to @specimen, notice: 'Material Was Added Successfully'
@@ -32,7 +32,7 @@ before_action :set_specimen, only: [:show, :edit, :update, :destroy]
       render :new
     end
   end
-  
+
   def update
     if @specimen.update(specimen_params)
       redirect_to @specimen, notice: 'Material was successfully updated.'
@@ -40,18 +40,18 @@ before_action :set_specimen, only: [:show, :edit, :update, :destroy]
       render 'edit'
     end
   end
-  
+
   def destroy
     @specimen.destroy
     redirect_to specimen_path, notice: "Material was deleted"
   end
-  
-  private 
+
+  private
 
   def set_specimen
     @specimen = Speciman.find(params[:id])
   end
-  
+
   def specimen_params
     params.require(:speciman).permit(:pass_or_fail, :test_plan, :cushion, :test_plan_id, :seat_model, :test_date, :vert_burn_length_front, :vert_burn_length_back, :horiz_burn_length_top, :horiz_burn_length_bottom, :weight_loss_percent, :weight_loss_lbs, :cover_config, :foam_a_material, :foam_a_percent, :foam_b_material, :foam_b_percent, :foam_c_material, :foam_c_percent, :foam_d_material, :foam_d_percent, :foam_e_material, :foam_e_percent, :decorative_insulation_part_number, :decorative_insulation_mfr, :backing, :adhesive_one, :adhesive_two, :fireblock_type, :fireblock_part_number, :fireblock_color, :fireblock_mfr, :flame_treatment, :fireblock_percent, :thermoplastic_part_number, :thermoplastic_mfr)
   end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,17 +1,17 @@
 class StaticController < ApplicationController
-  before_filter :authorize_admin, only: [:data_entry]
-  
+  before_action :authorize_admin, only: [:data_entry]
+
   def home
   end
-  
+
   def data_entry
   end
-  
+
   def instructions
   end
-  
+
   private
-  
+
   def authorize_admin
     return unless !current_user.admin?
     redirect_to root_path, alert: 'Admin Only!'
