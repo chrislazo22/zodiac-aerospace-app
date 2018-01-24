0c9ebd37c9168e8316558ba067e2d48be536b0bd
Christian Lazo <lazochristian.cl@gmail.com>
2018-01-23 20:46:41 -0800

    Updates for Rails 5

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,22 +1,22 @@
 class AdminController < Devise::RegistrationsController
-  before_filter :authorize_admin, only: [:create, :index, :new, :dashboard]
-  skip_before_filter :require_no_authentication, only: [:new, :create]
+  before_action :authorize_admin, only: [:create, :index, :new, :dashboard]
+  skip_before_action :require_no_authentication, only: [:new, :create]
   before_action :set_user, only: [:show, :destroy]
-  
+
   def dashboard
   end
-  
+
   def index
     @user = User.order("first_name").page(params[:page])
   end
-  
+
   def show
   end
-  
+
   def new
     super
   end
-  
+
   def create
     build_resource(sign_up_params)
     if resource.save
@@ -25,23 +25,23 @@ class AdminController < Devise::RegistrationsController
       render :new, notice: "Invalid Entry"
     end
   end
-  
+
   def destroy
     @user.destroy
     redirect_to admin_path, notice: "User Deleted Successfully"
   end
-  
-  
+
+
   private
   def authorize_admin
     return unless !current_user.admin?
     redirect_to root_path, alert: 'Admin Only!'
   end
-  
+
   def set_user
     @user = User.find(params[:id])
   end
-  
+
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin)
   end
